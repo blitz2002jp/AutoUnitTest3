fe05b02e7b44a369575943429b3c21be52ed05a3
blitz2002jp <blitz2002jp@gmail.com>
2024-06-19 14:42:50 +0900

    Initial Commit

---
+++ AutoUnitTest3/AutoUnitTest3App.swift
@@ -0,0 +1,17 @@
+//
+//  AutoUnitTest3App.swift
+//  AutoUnitTest3
+//
+//  Created by masazumi oeda on 2024/06/19.
+//
+
+import SwiftUI
+
+@main
+struct AutoUnitTest3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AutoUnitTest3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AutoUnitTest3
+//
+//  Created by masazumi oeda on 2024/06/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
